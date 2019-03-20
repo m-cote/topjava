00038cece1a45e9259f01a9d385d2bc793372e1c
Milan Kotykov <milankotykov@outlook.com>
2019-03-20 11:36:20 +0200

    HW06. fixed in memory rest controller tests

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -1,13 +1,8 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.*;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
-import ru.javawebinar.topjava.Profiles;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepositoryImpl;
@@ -19,27 +14,12 @@ import java.util.Collection;
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 
 public class InMemoryAdminRestControllerTest {
-    private static ConfigurableWebApplicationContext appCtx;
+    private static ConfigurableApplicationContext appCtx;
     private static AdminRestController controller;
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new XmlWebApplicationContext();
-        appCtx.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION);
-        appCtx.setConfigLocation("classpath:spring/spring-app.xml,classpath:spring/inmemory.xml");
-        appCtx.setServletContext(new MockServletContext(new ResourceLoader() {
-            @Override
-            public Resource getResource(final String location) {
-                return new FileSystemResource("src/main/resources" + location);
-            }
-
-            @Override
-            public ClassLoader getClassLoader() {
-                return getClassLoader();
-            }
-        }));
-        appCtx.refresh();
-
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml");
         System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
         controller = appCtx.getBean(AdminRestController.class);
     }
